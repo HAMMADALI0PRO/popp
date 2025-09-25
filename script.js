4af0aaf2ea4c4b3fa4247139143b331d068af404
HAMMADALI0PRO <hammadali1no@gmail.com>
2025-09-25 15:23:37 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const splashScreen = document.getElementById('splashScreen');
+    
+    setTimeout(function() {
+        window.location.href = "main.html";
+    }, 5000);
+});
