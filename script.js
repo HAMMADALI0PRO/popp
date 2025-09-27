d145b4bc71a782fce798544d5df3e2ee14fad836
HAMMADALI0PRO <hammadali1no@gmail.com>
2025-09-27 17:23:52 +0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const splashScreen = document.getElementById('splashScreen');
-    
-    setTimeout(function() {
-        window.location.href = "main.html";
-    }, 5000);
-});
